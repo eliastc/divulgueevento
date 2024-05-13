11c8cba652103fe842123b70e741c2fb40307970
Elias Calado <eliascalado@hotmail.com>
2024-05-13 11:37:03 -0300

    Projeto criado

---
+++ src/main/java/com/mp/divulgueevento/DivulgueEventoApplication.java
@@ -0,0 +1,13 @@
+package com.mp.divulgueevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivulgueEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivulgueEventoApplication.class, args);
+	}
+
+}
